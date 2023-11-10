6800f1f0b83bc69be3bdcc1f1727f72a781ad961
csm <csm009177@gmail.com>
2023-11-10 20:26:57 +0900

    fix 6

--- app3.js
+++ app3.js
@@ -64,17 +64,28 @@ const pokemonList = ["피카츄", "라이츄", "파이리", "꼬부기"];
 
 // 문항.6
 // 명단에 같은 이름(성씨제외)을 가진 학생이 두 명 있습니다. 이 중 마지막에 있는 학생의 위치를 찾아 출력하는 코드를 작성하세요.
+// 방법1
 const newstudentList = [ "김우진", "김현", "방승희", "오승민", "홍문기", "유승민" ];
 
+const smList = [];
 newstudentList.forEach(element => {
   if(element[1]==="승"&&element[2]==="민"){
-    console.log(element)
+    smList.push(element)
   }
 });
+console.log(smList.pop())
+// 방법2
+
 
 
 // 문항.7
 // 명단에서 성씨가 ‘김’씨인 학생의 이름을 찾아 출력하는 코드를 작성하세요.
+// studentList.forEach(element=>{
+//   if(element[0]==="김"){
+//     console.log(element)
+//   }
+// })
+
 
 // 문항.8
 // 기초데이터(studentList, pokemonList) 두 개의 명단을 합쳐 전체 학생 명단을 만들려고 합니다. 두 명단을 결합하는 코드를 작성하세요.
