6a1215d06e3047ba52c27939f2fe2371a7aa4fff
csm <csm009177@gmail.com>
2023-11-10 20:18:15 +0900

    6

--- app3.js
+++ app3.js
@@ -64,9 +64,13 @@ const pokemonList = ["피카츄", "라이츄", "파이리", "꼬부기"];
 
 // 문항.6
 // 명단에 같은 이름(성씨제외)을 가진 학생이 두 명 있습니다. 이 중 마지막에 있는 학생의 위치를 찾아 출력하는 코드를 작성하세요.
-const a = studentList.lastIndexOf("홍문기")
-console.log(a)
+const newstudentList = [ "김우진", "김현", "방승희", "오승민", "홍문기", "유승민" ];
 
+newstudentList.forEach(element => {
+  if(element[1]==="승"&&element[2]==="민"){
+    console.log(element)
+  }
+});
 
 
 // 문항.7
